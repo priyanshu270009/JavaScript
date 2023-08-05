593d0ac13432ee1e741155f135523531ee442769
priyanshu270009 <98266423+priyanshu270009@users.noreply.github.com>
2023-08-05 15:37:51 +0000

    variables

---
+++ .devcontainer/01_basics/first.js
@@ -0,0 +1,7 @@
+var AccountId=4;
+let AccountEmail= "priyanshu@google.com"
+AccountNo= 123123423;
+let Accountname
+console.log(AccountNo);
+
+console.table([AccountId, AccountEmail, AccountNo, Accountname]);
