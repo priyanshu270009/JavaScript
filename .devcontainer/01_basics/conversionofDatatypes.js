f4fb9a8d9e1ecf91fe49dee144b4bdf17831b6c7
priyanshu270009 <98266423+priyanshu270009@users.noreply.github.com>
2023-08-07 18:11:22 +0000

    conversion

--- .devcontainer/01_basics/conversionofDatatypes.js
+++ .devcontainer/01_basics/conversionofDatatypes.js
@@ -13,7 +13,7 @@
 //console.log(typeof score);
 //console.log(typeof(score));
 
-let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
