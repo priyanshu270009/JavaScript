43b7952ebd5ede63ae810819b30effa431dc99cc
priyanshu270009 <98266423+priyanshu270009@users.noreply.github.com>
2023-08-08 20:11:36 +0000

    strings methods

---
+++ .devcontainer/01_basics/Strings.js
@@ -0,0 +1,28 @@
+const myname= "Priyanshu"
+
+const MyNumber= "253445453";
+
+console.log(myname + MyNumber);
+
+console.log(`My name is ${myname} and my phone number is ${MyNumber}`);
+
+const gamename= new String('priyanshufc');
+
+//console.log("Priyanshu".charAt(9));
+
+console.log("priyanshu"[8]);
+
+//The const declaration declares block-scoped local variables. The value of a constant can't be changed through reassignment using the assignment operator, but if a constant is an object, its properties can be added, updated, or removed.
+
+const a = "priyansffgfggfdhu"
+const b = "sonidfgdfgdfg"
+if (a < b) {
+  console.log(`${a} is less than ${b}`);
+} 
+else if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} 
+
+else {
+  console.log(`${a} and ${b} are equal.`);
+}
