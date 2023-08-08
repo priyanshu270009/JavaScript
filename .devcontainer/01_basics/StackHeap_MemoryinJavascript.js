30750b1e98c0c38a905ce97410162db3f6567d59
priyanshu270009 <98266423+priyanshu270009@users.noreply.github.com>
2023-08-08 19:00:34 +0000

    stack&heap memory

---
+++ .devcontainer/01_basics/StackHeap_MemoryinJavascript.js
@@ -0,0 +1,21 @@
+let MyYoutubename="HiteshChoudhary"
+let anothername= MyYoutubename 
+
+anothername= "ChaiAurCode"
+
+//console.log(MyYoutubename);
+
+
+console.log(anothername);
+console.log(MyYoutubename);
+
+let userOne= {
+    email : "priyanshusoni@gmail.com",
+    upi: "priyanshu@paytm"
+}
+
+let userTwo= userOne
+userTwo.email= "ps2233232@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
