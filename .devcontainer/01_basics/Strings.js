487f30e35912ae5494d2f4b00ec9bae7a26c55f9
Priyanshu Soni <98266423+priyanshu270009@users.noreply.github.com>
2023-08-11 19:32:54 +0000

    strings methods

--- .devcontainer/01_basics/Strings.js
+++ .devcontainer/01_basics/Strings.js
@@ -61,3 +61,14 @@ console.log(url.replace('%20', '-'))
 console.log(url.includes('sundar'))
 
 console.log(gameName.split('-'));
+
+// index of method 
+"Blue Whale".indexOf("Blue"); // returns  0
+"Blue Whale".indexOf("Blute"); // returns -1
+"Blue Whale".indexOf("Whale", 0); // returns  5
+"Blue Whale".indexOf("Whale", 5); // returns  5
+"Blue Whale".indexOf("Whale", 7); // returns -1
+"Blue Whale".indexOf(""); // returns  0
+"Blue Whale".indexOf("", 9); // returns  9
+"Blue Whale".indexOf("", 10); // returns 10
+"Blue Whale".indexOf("", 11); // returns 10
